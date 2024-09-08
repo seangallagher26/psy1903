e6e97b075ad9205eff8d045b5cf71aef9c66b7d7
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-07 21:06:12 -0400

    week 1 practice code from videos

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,2 +1,19 @@
-console.log('Hello World!');
-console.log('Hello!');
+// Define a variable to hold our experiment name
+let experiment = 'Stroop';
+let trialCountMax = 20;
+let colors = ['red', 'green', 'blue'];
+
+// TODO: Randomize Colors
+alert('Welcome to the ' + experiment + 'experiment!');
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount); // Expected: 20
+
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 <= 15); // true

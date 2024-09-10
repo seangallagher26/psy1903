62af1e69e55ee6cbbed1d3c68565e1986688c3f8
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-10 17:03:45 -0400

    pset partA

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,12 @@
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
+
+let response1 = prompt('What is 2+5');
+alert('You Answered ' + response1);
+
+let response2 = prompt('What is 7+1');
+alert('You Answered ' + response2);
+
+let response3 = prompt('What is 2+2');
+alert('You Answered ' + response3);
+
+alert("Thank you for your participation!")

83d49bd278d226c1b0f2634277d5ceea4b72d218
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-10 17:21:23 -0400

    Math Response Time Exercise - Part C

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -1,21 +1,31 @@
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
 
+let number1 = Math.floor(Math.random() * 10) + 1;
+let number2 = Math.floor(Math.random() * 10) + 1;
+
 let start = Date.now();
-let response1 = prompt('What is 2 + 5');
+let response1 = prompt('What is ' + number1 + ' + ' + number2);
 let end = Date.now();
 let responseTime = ((end - start) / 1000)
 alert('You answered ' + response1 + ' in ' + responseTime + ' seconds');
 
+let number3 = Math.floor(Math.random() * 10) + 1;
+let number4 = Math.floor(Math.random() * 10) + 1;
+
 let start2 = Date.now();
-let response2 = prompt('What is 7 + 1');
+let response2 = prompt('What is ' + number3 + ' + ' + number4);
 let end2 = Date.now();
 let responseTime2 = ((end2 - start2) / 1000)
 alert('You answered ' + response2 + ' in ' + responseTime2 + ' seconds');
 
+let number5 = Math.floor(Math.random() * 10) + 1;
+let number6 = Math.floor(Math.random() * 10) + 1;
+
 let start3 = Date.now();
-let response3 = prompt('What is 2 + 2');
+let response3 = prompt('What is ' + number5 + ' + ' + number6);
 let end3 = Date.now()
 let responseTime3 = ((end3 - start3) / 1000)
 alert('You answered ' + response3 + ' in ' + responseTime3 + ' seconds');
 
 alert("Thank you for your participation!")
+

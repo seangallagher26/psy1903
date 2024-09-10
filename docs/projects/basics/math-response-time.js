d7280459b9998c0dfcd492bf85a15d206b6c3752
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-10 17:15:22 -0400

    math response time exercise partB

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -1,12 +1,21 @@
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
 
-let response1 = prompt('What is 2+5');
-alert('You Answered ' + response1);
+let start = Date.now();
+let response1 = prompt('What is 2 + 5');
+let end = Date.now();
+let responseTime = ((end - start) / 1000)
+alert('You answered ' + response1 + ' in ' + responseTime + ' seconds');
 
-let response2 = prompt('What is 7+1');
-alert('You Answered ' + response2);
+let start2 = Date.now();
+let response2 = prompt('What is 7 + 1');
+let end2 = Date.now();
+let responseTime2 = ((end2 - start2) / 1000)
+alert('You answered ' + response2 + ' in ' + responseTime2 + ' seconds');
 
-let response3 = prompt('What is 2+2');
-alert('You Answered ' + response3);
+let start3 = Date.now();
+let response3 = prompt('What is 2 + 2');
+let end3 = Date.now()
+let responseTime3 = ((end3 - start3) / 1000)
+alert('You answered ' + response3 + ' in ' + responseTime3 + ' seconds');
 
 alert("Thank you for your participation!")

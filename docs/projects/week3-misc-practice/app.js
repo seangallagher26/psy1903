62c2a10b51d3f22a9792b14d5074d50af7cf167a
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-17 17:06:09 -0400

    Misc Practice B

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -1,16 +1,29 @@
-let response = prompt('What is 4+6?');
-let correctAnswer = 'The answer is 10.'
+/* let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+
+let response = prompt('What is ' + num1 + ' + ' + num2 + ' ?');
+let correctAnswer = num1 + num2
 
 let feedback = ''
 
-if (response == 10) {
+if (response == correctAnswer) {
     feedback = 'You got it correct!';
-} else if (response > 8 && response < 12)
-// could also say else if (response == 9 || response == 11), but this is less clean
-{
-    feedback = 'You almost got it correct!';
+} else if (response == correctAnswer + 1 || response == correctAnswer - 1) {
+    feedback = 'You were close!';
 } else {
-    feedback = 'You got it incorrect.';
+    feedback = 'Incorrect.';
 }
 
-alert(feedback + 'The correct answer is 10!')
+alert(feedback + ' The correct answer is ' + correctAnswer + '!');
+*/
+
+let age = prompt('How old are you?');
+if (age < 12) {
+    alert('Child');
+}
+if (age >= 12 && age < 18) {
+    alert('Teenager');
+}
+if (age >= 18) {
+    alert('Adult');
+}

5af883a59bc0bea2f6aeddb5f0b5b58a098d4dd3
Sean Gallagher <seangallagher@college.harvard.edu>
2024-09-24 17:11:19 -0400

    convertTemp

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,5 +1,18 @@
-function celsiusToFahrenheit(celsius) {
-    let fahrenheit = celsius * 1.8 + 32;
+/* function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
     return fahrenheit;
 }
-console.log(celsiusToFahrenheit(10));
+console.log(celsiusToFahrenheit(10)); */
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'f') {
+        let fahrenheit = (temp * 1.8) + 32;
+        return fahrenheit;
+    } else {
+        let celsius = (temp - 32) / 1.8;
+        return celsius;
+    }
+
+}
+
+console.log(convertTemp(10, 'c'))

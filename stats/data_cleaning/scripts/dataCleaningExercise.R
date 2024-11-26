65608686451954ebca249d63704ca7a8560fe618
Sean Gallagher <seangallagher@college.harvard.edu>
2024-11-26 15:55:28 -0500

    RPlot

--- stats/data_cleaning/scripts/dataCleaningExercise.R
+++ stats/data_cleaning/scripts/dataCleaningExercise.R
@@ -4,8 +4,10 @@ dir.create("data_cleaning/output")
 dir.create("data_cleaning/scripts")
 dir.create("data_cleaning/data")
 setwd("~/Desktop/psy1903/stats/data_cleaning/scripts/")
+
 if (!require("pacman")) {install.packages("pacman"); require("pacman")} 
-p_load("tidyverse", "rstudioapi", "lme4", "emmeans", "psych", "corrplot", "jsonlite")
+
+p_load("tidyverse","rstudioapi","lme4","emmeans","psych","corrplot","jsonlite")
 
 iat_data1 <- read.csv("~/Desktop/psy1903/osfstorage-archive/gender-major-iat-2024-11-05-21-51-39.csv", header = TRUE, sep = ",", na.strings = "NA")
 str(iat_data1)
@@ -28,24 +30,19 @@ for(column_name in column_names) {
 str(iat_data2)
 
 calculate_IAT_dscore <- function(data) {
-  # Step 1: Filter out trials <300 & >3000
- tmp <- data[data$rt > 300 & data$rt < 3000,]
-  # Step 2: Separate congruent & incongruent
+  tmp <- data[data$rt > 300 & data$rt < 5000,]
   congruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "science or men" |
                             tmp$expectedCategoryAsDisplayed == "liberal arts or women",]
   incongruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "science or women" |
                             tmp$expectedCategoryAsDisplayed == "liberal arts or men",]
-  # Step 3: calculate mean & stdev
   congruent_means <- mean(congruent_trials$rt, na.rm = TRUE)
   incongruent_trials <- mean(incongruent_trials$rt, na.rm = TRUE)
   pooled_sd <- sd(tmp$rt, na.rm = TRUE)
-  # Step 4: Calculate D-Score
   dscore <- (congruent_means - incongruent_trials) / pooled_sd
   
   return(dscore)
 }
  calculate_IAT_dscore()
-  
  
  ## Set a variable called directory_path with the path to the location of your data csv files. This directory should *only* contain your raw participant csv data files and no other files.
  directory_path <- "~/Desktop/psy1903/osfstorage-archive"
@@ -55,11 +52,11 @@ calculate_IAT_dscore <- function(data) {
  
  ## Create an empty data frame called dScores that has two columns (IAT) or three columns (EST) and as many rows as you have data files (e.g., participants)
  ## IAT
- dScores <- data.frame(matrix(nrow = length(files_list), ncol = 2))
+ dScores <- data.frame(matrix(nrow = length(files_list), ncol = 4))
  
  ## Rename the default column names to something meaningful
  ## IAT
- colnames(dScores) <- c("participant_ID", "d_score")
+ colnames(dScores) <- c("participant_ID", "d_score","whichPrime","questionnaire")
  
  ## Initiate variable i to represent row numbers for each iteration, starting with 1
  i = 1
@@ -111,8 +108,82 @@ calculate_IAT_dscore <- function(data) {
  ## Reverse score if necessary
  rev_items <- c("question1","question3","question5","question7","question9")
  for (rev_item in rev_items) {
-   questionnaire[,rev_item] <- 8 - questionnaire[,rev_item]
+   questionnaire[,rev_item] <- 6 - questionnaire[,rev_item]
  }
  
  ## Calculate mean or sum score
  score <- rowMeans(questionnaire, na.rm = TRUE)
+
+ 
+ ## Initiate function called score_questionnaire that accepts a single argument called `data`. Within this function...
+ score_questionnaire <- function(data) {
+   json_data <- data[data$trialType == "Questionnaire", "response"]
+   questionnaire <- fromJSON(json_data)
+   questionnaire <- as.data.frame(lapply(questionnaire, as.numeric))
+   rev_items <- c("question1","question3","question5","question7","question9")
+   for (rev_item in rev_items) {
+     questionnaire[,rev_item] <- 6 - questionnaire[,rev_item]
+   }
+   score <- rowMeans(questionnaire, na.rm = TRUE)
+   return(score)
+}
+ 
+## Question 4
+ directory_path <- "~/Desktop/psy1903/osfstorage-archive"
+ files_list <- list.files(path = directory_path, pattern = "\\.csv$", full.names = TRUE)
+ dScores <- data.frame(matrix(nrow = length(files_list), ncol = 4))
+ colnames(dScores) <- c("participant_ID", "d_score", "questionnaire", "whichPrime")
+ 
+ score_questionnaire <- function(data) {
+   json_data <- data[data$trialType == "questionnaire", "response"]
+   questionnaire <- fromJSON(json_data)
+   questionnaire <- as.data.frame(lapply(questionnaire, as.numeric))
+   rev_items <- c("Q1","Q3","Q5","Q7","Q9")
+   for (rev_item in rev_items) {
+     questionnaire[,rev_item] <- 6 - questionnaire[,rev_item]
+   }
+   score <- rowMeans(questionnaire, na.rm = TRUE)
+   return(score)
+ }
+ 
+ 
+ calculate_IAT_dscore <- function(data) {
+   tmp <- data[data$rt > 300 & data$rt < 5000,]
+   congruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "science or men" |
+                             tmp$expectedCategoryAsDisplayed == "liberal arts or women",]
+   incongruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "science or women" |
+                               tmp$expectedCategoryAsDisplayed == "liberal arts or men",]
+   congruent_means <- mean(congruent_trials$rt, na.rm = TRUE)
+   incongruent_trials <- mean(incongruent_trials$rt, na.rm = TRUE)
+   pooled_sd <- sd(tmp$rt, na.rm = TRUE)
+   dscore <- (congruent_means - incongruent_trials) / pooled_sd
+   
+   return(dscore)
+ }
+ 
+ i = 1
+ 
+ file <- files_list[[1]]
+ for (file in files_list) {
+   tmp <- read.csv(file)
+   column_names <- c("expectedCategoryAsDisplayed", "expectedCategory", "leftCategory", "rightCategory")
+   for(column_name in column_names) {
+     tmp[,column_name] <- as.factor(tmp[,column_name])
+   }
+   tmp[, "whichPrime"] <- as.factor(tmp[,"whichPrime"])
+   participant_ID <- tools::file_path_sans_ext(basename(file))
+   whichPrime <- tmp[1,"whichPrime"]
+   dScores[i,"participant_ID"] <- participant_ID
+   dScores[i, "d_score"] <- calculate_IAT_dscore(tmp)
+   dScores[i, "whichPrime"] <- levels(whichPrime)[2]
+   dScores[i, "questionnaire"] <- score_questionnaire(tmp)
+   
+   rm(tmp)
+   i <- i + 1
+ }
+ 
+ dScores[,"whichPrime"] <- as.factor(dScores[,"whichPrime"])
+ dScores[,"d_score"] <- as.numeric(dScores[,"d_score"])
+ dScores[,"questionnaire"] <- as.numeric(dScores[,"questionnaire"])
+
+ 
